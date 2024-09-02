fb5df1cec38bc34e4c8e12120ed732880a53e8a9
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-02 16:17:55 +0200

    collegamento a Axios tramite CDN e stampato una email di prova in pagina presa dal link API di Boolean

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-
-
+/* 
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+ */
 const { createApp } = Vue ; 
-// Destructuring
+
 
     createApp({
         data() {
             return {
-                message: "Hello Vue!",
-                imgSrc: 'https://picsum.photos/200/300'
+                email:'',
+                
             };
+        },
+        created() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (resp) => {
+                this.email = resp.data.response })
         }
+
     }).mount('#app');
 
